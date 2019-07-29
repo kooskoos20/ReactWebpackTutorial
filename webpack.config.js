4ca61505cbfd48c4484da409bc19c76573fa2ca1
omkar6 <omkar@interleap.co>
2019-07-29 19:52:32 +0530

    updated comments in package.json

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ let path = require("path")  //path is an inbuilt nodejs plugin for paths
 module.exports = {          //exports it as a module(node js common practice)
     entry:"./src/index.js", //entry point for webpack, start transpiling and bundling from here
     output:{                //All output configurations
-      path: path.resolve(__dirname,'dist'),//Where to store outputs, std practice to store in dist
+      path: path.resolve(__dirname,'dist'),//Where to store outputs, std practice to store in dist resolve gives absolute path with arg1 as root whereas join just concatenates
       filename: "bundle.js",                // transpiled file name, available at root
     },    
     devServer:{                     //development mode specification
